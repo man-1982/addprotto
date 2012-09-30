8789305f8c642ee671306adfc663a8f14c1b6c5f
akochergin <man-1982@yandex.ru>
2012-09-30 21:46:40 +0400

    remove sitename and slogan from templates files

--- www/sites/all/themes/stroycenter/templates/page.tpl.php
+++ www/sites/all/themes/stroycenter/templates/page.tpl.php
@@ -78,19 +78,6 @@
       <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" /></a>
     <?php endif; ?>
 
-    <?php if ($site_name || $site_slogan): ?>
-      <hgroup id="name-and-slogan">
-        <?php if ($site_name): ?>
-          <h1 id="site-name">
-            <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
-          </h1>
-        <?php endif; ?>
-
-        <?php if ($site_slogan): ?>
-          <h2 id="site-slogan"><?php print $site_slogan; ?></h2>
-        <?php endif; ?>
-      </hgroup><!-- /#name-and-slogan -->
-    <?php endif; ?>
 
     <?php if ($secondary_menu): ?>
       <nav id="secondary-menu" role="navigation">
