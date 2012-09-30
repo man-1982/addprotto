deaed88ab0fac035a226c18fac415998ad311c66
akochergin <man-1982@yandex.ru>
2012-09-30 22:46:00 +0400

    remove logo form page tpl and maintaine page

--- www/sites/all/themes/stroycenter/templates/maintenance-page.tpl.php
+++ www/sites/all/themes/stroycenter/templates/maintenance-page.tpl.php
@@ -45,9 +45,9 @@
 
   <header id="header" role="banner">
 
-    <?php if ($logo): ?>
-      <a href="<?php print $base_path; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" /></a>
-    <?php endif; ?>
+<!--    --><?php //if ($logo): ?>
+<!--      <a href="--><?php //print $base_path; ?><!--" title="--><?php //print t('Home'); ?><!--" rel="home" id="logo"><img src="--><?php //print $logo; ?><!--" alt="--><?php //print t('Home'); ?><!--" /></a>-->
+<!--    --><?php //endif; ?>
 
 
 
--- www/sites/all/themes/stroycenter/templates/page.tpl.php
+++ www/sites/all/themes/stroycenter/templates/page.tpl.php
@@ -74,9 +74,9 @@
 
   <header id="header" role="banner">
 
-    <?php if ($logo): ?>
-      <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" /></a>
-    <?php endif; ?>
+<!--    --><?php //if ($logo): ?>
+<!--      <a href="--><?php //print $front_page; ?><!--" title="--><?php //print t('Home'); ?><!--" rel="home" id="logo"><img src="--><?php //print $logo; ?><!--" alt="--><?php //print t('Home'); ?><!--" /></a>-->
+<!--    --><?php //endif; ?>
 
 
     <?php if ($secondary_menu): ?>
