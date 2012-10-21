24a3c6c375f56386fbe5e755395106bba6b05fde
akochergin <man-1982@yandex.ru>
2012-10-21 20:27:01 +0400

    update module t17n

--- www/sites/all/modules/custom/t17n/js/t17n-accordion.js
+++ www/sites/all/modules/custom/t17n/js/t17n-accordion.js
@@ -9,7 +9,9 @@
       var accordionSelectror = Drupal.settings.t17n.accordion.selector;
       var accrodionOptions = {
         header      : 'li.t17n-level-0',
-        collapsible : true
+        collapsible : true,
+        active      : -1
+
       }
       $( accordionSelectror, context).not('.t17-accordion-process').accordion(accrodionOptions).addClass('t17-accordion-process');
       if(window.console) {
